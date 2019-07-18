60cf16b19da126216e2cac4e523dfacd71e3eec2
Tamara <Tamara@192.168.0.11>
2019-07-18 11:51:03 +0200

    R. Modificación del padre jugador

--- ticTacToe/src/Player.java
+++ ticTacToe/src/Player.java
@@ -1,33 +1,17 @@
-class Player {
+abstract class Player {
 
-	private Board board;
+	protected Board board;
 	
-	private Token token;
+	protected Token token;
 
 	Player(Board board, Token token) {
 		this.board = board;
 		this.token = token;
 	}
-
-	void put() {
-		CoordinateTicTacToe coordinate = new CoordinateTicTacToe();
-		do {
-			coordinate.read("Enter a coordinate to put a token:");
-		} while (!this.board.isEmpty(coordinate));
-		this.board.put(coordinate, this.token);
-	}
-
-	void move() {
-		CoordinateTicTacToe coordinate = new CoordinateTicTacToe();
-		CoordinateTicTacToe originCoordinate = new CoordinateTicTacToe();
-		do {
-			originCoordinate.read("Enter a coordinate to remove a token:");
-		} while (!this.board.isOccupied(originCoordinate, this.token));
-		do {
-			coordinate.read("Enter a coordinate to put a token:");
-		} while (!this.board.isEmpty(coordinate));
-		this.board.move(originCoordinate, coordinate);
-	}
+	
+	abstract void put();
+	
+	abstract void move();
 
 	Token getToken() {
 		return this.token;
