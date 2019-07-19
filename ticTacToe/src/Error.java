910cd1be4fb8022e31173fbd288201261b3f0da5
Tamara <Tamara@192.168.0.11>
2019-07-19 11:59:20 +0200

    A. Creado enum Error

---
+++ ticTacToe/src/Error.java
@@ -0,0 +1,17 @@
+enum Error {
+
+	NOT_EMPTY("The square is not empty"),
+	NOT_OWNER("There is not a token of yours"),
+	SAME_SQUARE("The origin and target squares are the same");
+	
+	private String message;
+	
+	Error(String message){
+		this.message = message;
+	}
+	
+	void write() {
+		new Console().writeln("ERROR!!! " + this.message);
+	}
+	
+}
