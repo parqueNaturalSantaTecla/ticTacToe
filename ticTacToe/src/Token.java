30b679670bc2830bc3432f21f62cf858b23835c1
Tamara <Tamara@192.168.0.11>
2019-07-29 22:02:16 +0200

    A. Nuevo enum Token

---
+++ ticTacToe/src/Token.java
@@ -0,0 +1,16 @@
+enum Token {
+	
+	TOKEN_X('X'),
+	TOKEN_O('O');
+	
+	private char character;
+	
+	Token(char character){
+		this.character = character;
+	}
+	
+	void write() {
+		new Console().write(this.character);
+	}
+	
+}
